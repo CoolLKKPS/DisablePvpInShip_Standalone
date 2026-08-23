30c80d52a66bc7a91db13b93d26f46238504982e
CoolLKK PS <PSKyle5926@outlook.com>
2026-08-23 10:38:27 +0800

    Tweak info

--- DisablePvpInShip_Standalone/Properties/AssemblyInfo.cs
+++ DisablePvpInShip_Standalone/Properties/AssemblyInfo.cs
@@ -3,7 +3,6 @@ using System.Reflection;
 
 [assembly: AssemblyVersion(DisablePvpInShip_StandalonePlugin.PLUGIN_VERSION_FULL)]
 [assembly: AssemblyCompany(DisablePvpInShip_StandalonePlugin.PLUGIN_NAME)]
-[assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyDescription("HostFixes DisablePvpInShip Standalone")]
 [assembly: AssemblyFileVersion(DisablePvpInShip_StandalonePlugin.PLUGIN_VERSION_FULL)]
 [assembly: AssemblyInformationalVersion(DisablePvpInShip_StandalonePlugin.PLUGIN_VERSION)]
